447a1d46c5408627fba8da5882bdd21e4c0f74cc
Manjunath D <82793670+ManjunathD620@users.noreply.github.com>
2022-09-02 19:25:13 +0530

    Update Main.java

--- bunchbycycles/Main.java
+++ bunchbycycles/Main.java
@@ -10,22 +10,50 @@ public class Main {
   static CountsByUsage countBatteriesByUsage(int[] cycles) {
     CountsByUsage counts = new CountsByUsage();
 
+
+
+
         // Approach
         // Linearly Iterate all the values one by one
         //  Checking the base condition to classify the Batteries
         //  Time Complexity with respect to this code --> O(N) , where N-size of array , because we are using linear time
         //  Space Complexity is O(1) because No Extra space is used
 
+
+        // Additional thing is we can group the batteries based on their  life cycle 
+        // We can use this data to identify the batteries and also we can store the index as well(Not implemented)
+        ArrayList<ArrayList<Integer>> Grouped = new ArrayList<>();
+        ArrayList<Integer> lowBattery = new ArrayList<>();
+        ArrayList<Integer> mediumBattery = new ArrayList<>();
+        ArrayList<Integer> highBattery = new ArrayList<>();
+
+
         for(int i=0;i< cycles.length;i++){
             if(cycles[i] < 400)
+            {
                 counts.lowCount = counts.lowCount+1;
+                lowBattery.add(cycles[i]);
+            }
             else if(cycles[i]>=400 && cycles[i]<=919)
+            {
                 counts.mediumCount = counts.mediumCount + 1;
-            else if(cycles[i] >= 920)
+                mediumBattery.add(cycles[i]);
+            }
+            else if(cycles[i] >= 920){
                 counts.highCount = counts.highCount + 1;
+                highBattery.add(cycles[i]);
+            }
+
             else //Checking if the value is less than 0 ( Boundary Condition )
                 System.out.println("Invalid Cycle");
         }
+
+        Grouped.add(lowBattery);
+        Grouped.add(mediumBattery);
+        Grouped.add(highBattery);
+
+    //        for(int i=0;i<3;i++)
+    //            System.out.println(Grouped.get(i));
 
         return counts;
   }
