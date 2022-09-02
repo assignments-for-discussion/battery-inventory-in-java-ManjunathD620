f81ad23f2753081cbe5261eea6df7652932f1396
Manjunath D <82793670+ManjunathD620@users.noreply.github.com>
2022-09-02 19:10:35 +0530

    Update Main.java

--- bunchbycycles/Main.java
+++ bunchbycycles/Main.java
@@ -9,15 +9,33 @@ public class Main {
 
   static CountsByUsage countBatteriesByUsage(int[] cycles) {
     CountsByUsage counts = new CountsByUsage();
-    return counts;
+
+        // Approach
+        // Linearly Iterate all the values one by one
+        //  Checking the base condition to classify the Batteries
+        //  Time Complexity with respect to this code --> O(N) , where N-size of array , because we are using linear time
+        //  Space Complexity is O(1) because No Extra space is used
+
+        for(int i=0;i< cycles.length;i++){
+            if(cycles[i] < 400)
+                counts.lowCount = counts.lowCount+1;
+            else if(cycles[i]>=400 && cycles[i]<=919)
+                counts.mediumCount = counts.mediumCount + 1;
+            else if(cycles[i] >= 920)
+                counts.highCount = counts.highCount + 1;
+            else //Checking if the value is less than 0 ( Boundary Condition )
+                System.out.println("Invalid Cycle");
+        }
+
+        return counts;
   }
 
   static void testBucketingByNumberOfCycles() {
     System.out.println("Counting batteries by usage cycles...\n");
     CountsByUsage counts = countBatteriesByUsage(new int[] {100, 300, 500, 600, 900, 1000});
-    assert(counts.lowCount == 2);
-    assert(counts.mediumCount == 3);
-    assert(counts.highCount == 1);
+    assert (counts.lowCount == 2) : "Low Count is Wrong";
+    assert (counts.mediumCount == 3) : "Medium Count is Wrong";
+    assert (counts.highCount == 1) : " High Count is wrong";
     System.out.println("Done counting :)\n");
   }
 
